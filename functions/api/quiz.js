829ffd580bc2a5972438e59bd55623599cc5b299
AI007PATROL <aids33.rpsit.org@gmail.com>
2025-09-15 23:34:27 +0530

    Update quiz.js

--- functions/api/quiz.js
+++ functions/api/quiz.js
@@ -1,11 +1,9 @@
 // functions/questions.js
 export const onRequest = async ({ request }) => {
   try {
-    // Load all questions from your bundled JSON
     const resp = await fetch(new URL('/data_questions.json', request.url).toString());
     const qs = await resp.json();
 
-    // Return all questions, frontend will randomize/pick 20
     return new Response(JSON.stringify(qs), {
       headers: { "Content-Type": "application/json" },
       status: 200
